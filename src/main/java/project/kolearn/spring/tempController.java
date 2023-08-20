d04320745a37025c4e2b46a63643f0e965663a21
coq-j01 <chaebinesco@gmail.com>
2023-08-20 19:14:10 +0900

    image출력 문제 해결!

--- src/main/java/project/kolearn/spring/tempController.java
+++ src/main/java/project/kolearn/spring/tempController.java
@@ -1,15 +1,36 @@
 package project.kolearn.spring;
 
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.net.MalformedURLException;
 
 @Controller
 //@RequiredArgsConstructor
 public class tempController {
 
-    @GetMapping("/")
-    public String index(){
-        return "index";
+    @GetMapping("camera")
+    public String camera() {
+        return "camera";
     }
 
+    @GetMapping("trans")
+    public String trans() {
+        return "trans";
+    }
+
+    @GetMapping("dict")
+    public String dict() {
+        return "dict";
+    }
+
+    @GetMapping("quiz")
+    public String quiz() {
+        return "quiz";
+    }
 }
